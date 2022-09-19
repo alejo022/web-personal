57601ffcb05f7c21dd7fcc733d5b61c6af1df314
Paginas Cetiia <postmaster@cetiia.com.co>
2022-09-19 12:00:25 -0500

    app estable con juego incluido

--- public/js/piedra_papel_tijera.js
+++ public/js/piedra_papel_tijera.js
@@ -14,90 +14,90 @@ const imgMaquina = document.getElementById("img-maquina");
 const imgUsuario = document.getElementById("img-usuario");
 
 botonPiedra.addEventListener("click", () => {
-  /* console.log("piedra"); */
-  play(piedra);
+    /* console.log("piedra"); */
+    play(piedra);
 });
 
 botonPapel.addEventListener("click", () => {
-  /* console.log("papel"); */
-  play(papel);
+    /* console.log("papel"); */
+    play(papel);
 });
 
 botonTijera.addEventListener("click", () => {
-  /* console.log("tijera"); */
-  play(tijeras);
+    /* console.log("tijera"); */
+    play(tijeras);
 });
 
 function play(OpcionUsuario) {
 
-  imgUsuario.src = "img/piedra_papel_tijera/" + OpcionUsuario + ".png";
+    imgUsuario.src = "img/piedra_papel_tijera/" + OpcionUsuario + ".png";
 
-  textoResultado.innerHTML = "Jugando...";
+    textoResultado.innerHTML = "Jugando...";
 
-  const intervalo = setInterval(function () {
-    const OpcionMaquina = calcularOpcionMaquina();
-    imgMaquina.src = "img/piedra_papel_tijera/" + OpcionMaquina + ".png";
-  }, 200);
+    const intervalo = setInterval(function () {
+        const OpcionMaquina = calcularOpcionMaquina();
+        imgMaquina.src = "img/piedra_papel_tijera/" + OpcionMaquina + ".png";
+    }, 200);
 
-  setTimeout(function () {
+    setTimeout(function () {
 
-    clearInterval(intervalo);
+        clearInterval(intervalo);
 
-    const OpcionMaquina = calcularOpcionMaquina();
+        const OpcionMaquina = calcularOpcionMaquina();
 
-    /* console.log (OpcionMaquina); */
+        /* console.log (OpcionMaquina); */
 
-    const resultado = calcularResultado(OpcionUsuario, OpcionMaquina)
+        const resultado = calcularResultado(OpcionUsuario, OpcionMaquina)
 
-    imgMaquina.src = "img/piedra_papel_tijera/" + OpcionMaquina + ".png";
+        imgMaquina.src = "img/piedra_papel_tijera/" + OpcionMaquina + ".png";
 
-    switch (resultado) {
-      case empate:
-        /* alert(OpcionMaquina+"Hubo un empate"); */
-        textoResultado.innerHTML = "Hubo un empate";
-        break;
-      case gano:
-        /* alert(OpcionMaquina+"Ganaste"); */
-        textoResultado.innerHTML = "Ganaste";
-        break;
-      case perdio:
-        /* alert(OpcionMaquina+"Perdiste"); */
-        textoResultado.innerHTML = "Perdiste";
-        break;
-    }
-  }, 2000);
+        switch (resultado) {
+            case empate:
+                /* alert(OpcionMaquina+"Hubo un empate"); */
+                textoResultado.innerHTML = "Hubo un empate";
+                break;
+            case gano:
+                /* alert(OpcionMaquina+"Ganaste"); */
+                textoResultado.innerHTML = "Ganaste";
+                break;
+            case perdio:
+                /* alert(OpcionMaquina+"Perdiste"); */
+                textoResultado.innerHTML = "Perdiste";
+                break;
+        }
+    }, 2000);
 
 }
 
 function calcularOpcionMaquina() {
-  const numero = Math.floor(Math.random() * 3);
-  switch (numero) {
-    case 0:
-      return piedra;
-    case 1:
-      return papel;
-    case 2:
-      return tijeras;
-  }
+    const numero = Math.floor(Math.random() * 3);
+    switch (numero) {
+        case 0:
+            return piedra;
+        case 1:
+            return papel;
+        case 2:
+            return tijeras;
+    }
 }
 
 function calcularResultado(OpcionUsuario, OpcionMaquina) {
-  if (OpcionUsuario === OpcionMaquina) {
-    return empate
-  } else if (OpcionUsuario === piedra) {
+    if (OpcionUsuario === OpcionMaquina) {
+        return empate
+    } else if (OpcionUsuario === piedra) {
 
-    if (OpcionMaquina === papel) return perdio;
-    if (OpcionMaquina === tijeras) return gano;
+        if (OpcionMaquina === papel) return perdio;
+        if (OpcionMaquina === tijeras) return gano;
 
-  } else if (OpcionUsuario === papel) {
+    } else if (OpcionUsuario === papel) {
 
-    if (OpcionMaquina === piedra) return gano;
-    if (OpcionMaquina === tijeras) return perdio;
+        if (OpcionMaquina === piedra) return gano;
+        if (OpcionMaquina === tijeras) return perdio;
 
-  } else if (OpcionUsuario === tijeras) {
+    } else if (OpcionUsuario === tijeras) {
 
-    if (OpcionMaquina === piedra) return perdio;
-    if (OpcionMaquina === papel) return gano;
+        if (OpcionMaquina === piedra) return perdio;
+        if (OpcionMaquina === papel) return gano;
 
-  }
+    }
 }
